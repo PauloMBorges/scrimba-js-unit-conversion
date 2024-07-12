2eb6e5aaa25ae221d28f087bc1ce6dbf7cc7e83a
PauloMBorges <paulodemeloborges@gmail.com>
2024-07-12 12:13:14 -0300

    Add convert() function
    - gets user input
    - checks if it's higher than the MAX_VALUE (if it is, return and displays warning message)
    - if it's not '', converts all values and displays it

--- script.js
+++ script.js
@@ -1,5 +1,41 @@
-/*
-1 meter = 3.281 feet
-1 liter = 0.264 gallon
-1 kilogram = 2.204 pound
-*/
+const meterToFeet =  3.2808399
+const literToGallon =  0.264172052
+const kiloToPound =  2.204
+const MAX_VALUE = 10000000
+
+let convertBtn = document.getElementById("convert-btn")
+let inputEl = document.getElementById("input-el")
+let lengthEl = document.getElementById("length-el")
+let volumeEl = document.getElementById("volume-el")
+let massEl = document.getElementById("mass-el")
+let warningEl = document.getElementById("warning-el")
+
+convertBtn.addEventListener("click", convert)
+
+function convert() {
+    let inputValue = inputEl.value /* get user input */
+
+    if(inputValue > MAX_VALUE) {
+        warningEl.style.display = "inline"
+        warningEl.innerText = `The maximum allowed value is ${MAX_VALUE.toLocaleString()}.`
+        return
+    } else {
+        warningEl.style.display = "none"
+    }
+
+    if(inputValue !== '') {
+        let feetValue = (inputValue * meterToFeet).toFixed(3)
+        let meterValue = (inputValue / meterToFeet).toFixed(3)
+        lengthEl.innerHTML =  `<p id="length-el">${inputValue} meters = ${feetValue} feet | ${inputValue} feet = ${meterValue} meters</>`
+
+        let gallonValue = (inputValue * literToGallon).toFixed(3)
+        let literValue = (inputValue / literToGallon).toFixed(3)
+        volumeEl.innerHTML = `<p id="volume-el">${inputValue} liters = ${gallonValue} gallons | ${inputValue} gallons = ${literValue} liters</p>`
+        
+        let kiloValue = (inputValue * kiloToPound).toFixed(3)
+        let poundValue = (inputValue / kiloToPound).toFixed(3)
+        massEl.innerHTML = `<p id="mass-el">${inputValue} kilos = ${kiloValue} pounds | ${inputValue} pounds = ${poundValue} kilos</p>`
+    }
+
+}   
+
